a84ca3c42eaf77e761722a1800f0a97ef51f2832
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-03 16:05:37 +0000

    Fichero de la clase Bibliography creado

--- lib/bibliography.rb
+++ lib/bibliography.rb
@@ -2,6 +2,25 @@ require "gem/version"
 
 module Bibliography
   module Gem
-    # Your code goes here...
+    
+    class Bibliography # Clase para el desarrollo de referencias bibliográficas.
+    
+      attr_accessor :author, :title, :series, :editorial, :edition_number, :date, :isbn
+    
+      #-----> MÉTODOS ELEMENTALES DE LA CLASE.
+    
+      def initialize(author, title, series, editorial, edition_number, date, isbn) # Constructor de la clase.
+    
+      # Declaración e inicialización de las variables de instancia.
+      @author = author
+      @title = title
+      @series = series
+      @editorial = editorial
+      @edition_number = edition_number
+      @date = date
+      @isbn = isbn
+    
+      end
+    end
   end
 end
