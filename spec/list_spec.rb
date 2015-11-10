0cea9e51d243cbae46b8a2c2bac0402642a7108a
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-10 19:16:46 +0000

    Creada una prueba para comprobar la existencia de una lista bibliográfica [REALIZADA CON ÉXITO]

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -71,13 +71,23 @@ end
 
 describe "Pruebas para una lista bibliográfica" do
     before :each do
-        @list = List.new(nil)
+        @bibliography_list = List.new(nil)
         Node = Struct.new(:value, :next)
         
-        @node1 = Node.new(Bibliography.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf;", "4 edition", "July 7, 2013", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"]),nil)
-        @node2 = Node.new(Bibliography.new(["Scott Chacon"], "Pro Git 2009th Edition.", "(Pro).", "Apress;", "2009 edition", "(August 27, 2009).", ["ISBN-13: 978- 1430218333.", "ISBN-10: 1430218339."]),nil)
-        @node3 = Node.new(Bibliography.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language.", 0, "O’Reilly Media;", "1 edition", "(February 4, 2008).", ["ISBN-10: 0596516177.", "ISBN-13: 978-0596516178."]),nil)
-        @node4 = Node.new(Bibliography.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy."],"The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby).", "Pragmatic Bookshelf;", "1 edition", "(December 25, 2010).", ["ISBN-10: 1934356379.", "ISBN-13: 978-1934356371."]),nil)
-        @node5 = Node.new(Bibliography.new(["Richard E."], "Silverman Git Pocket Guide", 0, "O’Reilly Media;", "1 edition", "(August 2, 2013).", ["ISBN-10: 1449325866.", "ISBN-13: 978-1449325862."]),nil)
+        @reference1 = Node.new(Bibliography.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf;", "4 edition", "July 7, 2013", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"]),nil)
+        @reference2 = Node.new(Bibliography.new(["Scott Chacon"], "Pro Git 2009th Edition.", "(Pro).", "Apress;", "2009 edition", "(August 27, 2009).", ["ISBN-13: 978- 1430218333.", "ISBN-10: 1430218339."]),nil)
+        @reference3 = Node.new(Bibliography.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language.", 0, "O’Reilly Media;", "1 edition", "(February 4, 2008).", ["ISBN-10: 0596516177.", "ISBN-13: 978-0596516178."]),nil)
+        @reference4 = Node.new(Bibliography.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy."],"The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby).", "Pragmatic Bookshelf;", "1 edition", "(December 25, 2010).", ["ISBN-10: 1934356379.", "ISBN-13: 978-1934356371."]),nil)
+        @reference5 = Node.new(Bibliography.new(["Richard E."], "Silverman Git Pocket Guide", 0, "O’Reilly Media;", "1 edition", "(August 2, 2013).", ["ISBN-10: 1449325866.", "ISBN-13: 978-1449325862."]),nil)
+    
+        @bibliography_list.insert(@reference1)
+        @bibliography_list.insert(@reference2)
+        @bibliography_list.insert(@reference3)
+        @bibliography_list.insert(@reference4)
+        @bibliography_list.insert(@reference5)
+    end
+    
+    it "Existe una lista bibliográfica" do
+        expect(@bibliography_list.empty?).to eq(false)
     end
 end
