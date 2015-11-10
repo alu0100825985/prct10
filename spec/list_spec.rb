0808b348c2da0a125372c7afdc8ba255723a57fb
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-10 19:32:55 +0000

    Creada una prueba para comprobar los fechas de publicación de la lista bibliográfica [REALIZADA CON ÉXITO]

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -75,10 +75,10 @@ describe "Pruebas para una lista bibliográfica" do
         Node = Struct.new(:value, :next)
         
         @reference1 = Node.new(Bibliography.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf;", "4 edition", "July 7, 2013", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"]),nil)
-        @reference2 = Node.new(Bibliography.new(["Scott Chacon"], "Pro Git 2009th Edition.", "(Pro).", "Apress;", "2009 edition", "(August 27, 2009).", ["ISBN-13: 978- 1430218333.", "ISBN-10: 1430218339."]),nil)
+        @reference2 = Node.new(Bibliography.new(["Scott Chacon"], "Pro Git 2009th Edition.", "(Pro).", "Apress;", "2009 edition", "(August 27, 2009)", ["ISBN-13: 978- 1430218333.", "ISBN-10: 1430218339."]),nil)
         @reference3 = Node.new(Bibliography.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language.", 0, "O’Reilly Media;", "1 edition", "(February 4, 2008).", ["ISBN-10: 0596516177.", "ISBN-13: 978-0596516178."]),nil)
         @reference4 = Node.new(Bibliography.new(["David Chelimsky", "Dave Astels", "Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy."],"The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby).", "Pragmatic Bookshelf;", "1 edition", "(December 25, 2010).", ["ISBN-10: 1934356379.", "ISBN-13: 978-1934356371."]),nil)
-        @reference5 = Node.new(Bibliography.new(["Richard E."], "Silverman Git Pocket Guide", 0, "O’Reilly Media;", "1 edition", "(August 2, 2013).", ["ISBN-10: 1449325866.", "ISBN-13: 978-1449325862."]),nil)
+        @reference5 = Node.new(Bibliography.new(["Richard E."], "Silverman Git Pocket Guide", 0, "O’Reilly Media;", "1 edition", "(August 2, 2013)", ["ISBN-10: 1449325866.", "ISBN-13: 978-1449325862."]),nil)
     
         @bibliography_list.insert(@reference1)
         @bibliography_list.insert(@reference2)
@@ -92,6 +92,13 @@ describe "Pruebas para una lista bibliográfica" do
     end
     
     it "Existe una referencia de la lista que tiene como autor a Richard E." do
-        expect(@bibliography_list.extract.next).to eq(@reference4)
+        expect(@bibliography_list.extract.value.author[0]).to eq("Richard E.")
+    end
+    
+    it "Existe una referencia de la lista que tiene como autor a Richard E." do
+        @bibliography_list.extract
+        @bibliography_list.extract
+        @bibliography_list.extract
+        expect(@bibliography_list.extract.value.get_date()).to eq("(August 27, 2009)")
     end
 end
