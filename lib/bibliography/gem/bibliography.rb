2ddc2b6404bc25da3b1bf080ec1468452824b683
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-16 16:16:12 +0000

    Modificada la clase bibliography.rb para estructurar de una manera más ordenada y clara la jerarquía de clases

--- lib/bibliography/gem/bibliography.rb
+++ lib/bibliography/gem/bibliography.rb
@@ -1,25 +1,18 @@
-    class Bibliography # Clase para el desarrollo de referencias bibliográficas.
+    class References # Clase para el desarrollo de referencias.
     
-      attr_accessor :author, :title, :series, :editorial, :edition_number, :date, :isbn
+      attr_accessor :author, :title, :editorial, :date
     
       #-----> MÉTODOS ELEMENTALES DE LA CLASE.
     
-      def initialize(author, title, series, editorial, edition_number, date, isbn) # Constructor de la clase.
+      def initialize(author, title, editorial, date) # Constructor de la clase.
     
       # Declaración e inicialización de las variables de instancia.
       @author = author
       @title = title
-      @series = series
       @editorial = editorial
-      @edition_number = edition_number
       @date = date
-      @isbn = isbn
       
       end
-      
-      def formatted_reference() # Método para mostrar la referencia bajo formato.
-        "AUTORES: #{@author[0]}, #{@author[1]}, #{@author[2]} | TÍTULO: #{@title} | SERIE: #{@series} | EDITORIAL: #{@editorial} | NÚMERO DE EDICIÓN: #{@edition_number} | FECHA DE PUBLICACIÓN: #{@date} | ISBN: #{@isbn[0]}, #{@isbn[1]}"
-      end 
       
       #-----> MÉTODOS GETTERS DE LA CLASE.
       
@@ -31,80 +24,71 @@
         return @title
       end
       
-      def get_series() # Método para la devolución de series.
-        return @series
-      end
-      
       def get_editorial() # Método para la devolución de editorial.
         return @editorial
-      end
-      
-      def get_edition_number() # Método para la devolución de edition_number.
-        return @edition_number
       end
       
       def get_date() # Método para la devolución de date.
         return @date
       end
-      
-      def get_isbn() # Método para la devolución de isbn.
-        return @isbn
-      end
     end
-  
-    class Periodicals # Clase para representar publicaciones periódicas.
-  
-      attr_accessor :author, :title
+    
+    class Bibliography < References # Clase para el desarrollo de referencias bibliográficas. Herencia de References.
+    
+      attr_accessor :series, :edition_number, :isbn
     
       #-----> MÉTODOS ELEMENTALES DE LA CLASE.
     
-      def initialize(author, title) # Constructor de la clase.
+      def initialize(series, edition_number, isbn) # Constructor de la clase.
     
       # Declaración e inicialización de las variables de instancia.
-      @author = author
-      @title = title
+      @series = series
+      @edition_number = edition_number
+      @isbn = isbn
       
       end
     end
     
-    class Magazine_Articles < Periodicals # Clase para representar artículos de revistas
+    class Magazine_Articles < References # Clase para representar artículos de revistas
     
-      attr_accessor :magazine
+      attr_accessor :magazine, :issn
     
       #-----> MÉTODOS ELEMENTALES DE LA CLASE.
     
-      def initialize(magazine) # Constructor de la clase.
+      def initialize(magazine, issn) # Constructor de la clase.
     
       # Declaración e inicialización de las variables de instancia.
       @magazine = magazine
+      @issn = issn
       
       end
     end
     
-    class Newspaper_Articles < Periodicals # Clase para representar artículos de periódicos.
+    class Newspaper_Articles < References # Clase para representar artículos de periódicos.
     
-      attr_accessor :newspaper
+      attr_accessor :newspaper, :print
     
       #-----> MÉTODOS ELEMENTALES DE LA CLASE.
     
-      def initialize(newspaper) # Constructor de la clase.
+      def initialize(newspaper, print) # Constructor de la clase.
     
       # Declaración e inicialización de las variables de instancia.
       @newspaper = newspaper
+      @print = print
       
       end
     end
     
-    class Electronic_Document < Periodicals # Clase para representar documentos electrónicos.
+    class Electronic_Document < References # Clase para representar documentos electrónicos.
     
-      attr_accessor :website
+      attr_accessor :url
     
       #-----> MÉTODOS ELEMENTALES DE LA CLASE.
     
-      def initialize(website) # Constructor de la clase.
+      def initialize(url) # Constructor de la clase.
     
       # Declaración e inicialización de las variables de instancia.
-      @website = website
+      @url = url
       
       end
     end
