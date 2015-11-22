ff87388c64566e7dcb271d25a78def05668e7289
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 16:44:57 +0000

    Expectativa para hacer una comparación del tipo mayor o igual que [REALIZADA CON ÉXITO]

--- spec/bibliography_spec.rb
+++ spec/bibliography_spec.rb
@@ -10,18 +10,23 @@ describe References do
     before :each do
       @references1 = References.new("Stan Rebick", "Ruby Modules", "Editorik", "23 January, 2015", "264")
       @references2 = References.new("Andy Coleman", "Ruby World", "Panemo", "15 April, 2010", "323")
+      @references3 = References.new("Rubok Anthy", "Ruby Computer", "Carco", "12 May, 1995", "517")
     end
     
-    it "La referencia de Stan Rebick tiene menos páginas que la referencia de Ricky Seawton" do
+    it "La referencia de Stan Rebick es menor que la referencia de Ricky Seawton" do
       expect(@references1 < @references).to eq(true)
     end
     
-    it "La referencia de Andy Coleman tiene menos o igual número de páginas que la referencia de Ricky Seawton" do
+    it "La referencia de Andy Coleman es menor o igual que la referencia de Ricky Seawton" do
       expect(@references2 <= @references).to eq(true)
     end
     
-    it "La referencia de Andy Coleman tiene igual número de páginas que la referencia de Ricky Seawton" do
+    it "La referencia de Andy Coleman es igual que la referencia de Ricky Seawton" do
       expect(@references2 == @references).to eq(true)
+    end
+    
+    it "La referencia de Andy Coleman es mayor o igual que la referencia de Ricky Seawton" do
+      expect(@references2 >= @references).to eq(true)
     end
   end
   
