63aaa6e6a2e7cab5ec1010676a54db0ad1e8bc51
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 18:33:35 +0000

    Expectativa para comprobar el método any? [REALIZADA CON ÉXITO]

--- spec/double_list_spec.rb
+++ spec/double_list_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 require './lib/bibliography/gem/double_list.rb'
-
-describe "Pruebas para una lista con nodos simples" do
-    before :each do
-        @doublelist = Double_List.new(nil,nil)
-    end
     
-    describe "Haciendo enumerable la lista doblemente enlazada" do
-        before :each do
-            @doublelist.insert(10)
-            @doublelist_empty = Double_List.new(nil,nil)
-        end
-        it "Comprobrando el metodo all?" do
-            expect(@doublelist.all?).to eq(true)
-            expect(@doublelist_empty.all?).to eq(false)
-        end
+describe "Haciendo enumerable la lista doblemente enlazada" do
+    it "Comprobrando el metodo all?" do
+        @node = Node.new(nil,10,nil)
+        @doublelist = Double_List.new(@node, @node)
+        @doublelist_empty = Double_List.new(nil,nil)
+        expect(@doublelist.all?).to eq(true)
+        expect(@doublelist_empty.all?).to eq(false)
     end
+    it "Comprobrando el metodo any?" do
+        @node = Node.new(nil,10,nil)
+        @doublelist = Double_List.new(@node, nil)
+        @doublelist_empty = Double_List.new(nil,nil)
+        expect(@doublelist.any?).to eq(true)
+        expect(@doublelist_empty.any?).to eq(false)
+    end 
 end
