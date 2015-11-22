6752960b2751f7d18c3852a8cf9d864364bf3bfb
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 19:09:33 +0000

    Expectativa para comprobar el método count [REALIZADA CON ÉXITO]

--- spec/double_list_spec.rb
+++ spec/double_list_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 require './lib/bibliography/gem/double_list.rb'
     
-describe "Haciendo enumerable la lista doblemente enlazada" do
-    it "Comprobrando el metodo all?" do
-        @node = Node.new(nil,10,nil)
-        @doublelist = Double_List.new(@node, @node)
-        @doublelist_empty = Double_List.new(nil,nil)
-        expect(@doublelist.all?).to eq(true)
-        expect(@doublelist_empty.all?).to eq(false)
+describe Double_List do
+    before :each do 
+        @doublelist = Double_List.new(nil,nil)
     end
-    it "Comprobrando el metodo any?" do
-        @node = Node.new(nil,10,nil)
-        @doublelist = Double_List.new(@node, nil)
-        @doublelist_empty = Double_List.new(nil,nil)
-        expect(@doublelist.any?).to eq(true)
-        expect(@doublelist_empty.any?).to eq(false)
-    end 
+    
+    describe "Haciendo enumerable la lista doblemente enlazada" do
+        
+        it "Comprobrando el metodo all?" do
+            @node = Node.new(nil,10,nil)
+            @doublelist_all = Double_List.new(@node, @node)
+            @doublelist_empty = Double_List.new(nil,nil)
+            expect(@doublelist_all.all?).to eq(true)
+            expect(@doublelist_empty.all?).to eq(false)
+        end
+        it "Comprobrando el metodo any?" do
+            @node = Node.new(nil,10,nil)
+            @doublelist_any = Double_List.new(@node, nil)
+            @doublelist_empty = Double_List.new(nil,nil)
+            expect(@doublelist_any.any?).to eq(true)
+            expect(@doublelist_empty.any?).to eq(false)
+        end 
+        it "Comprobrando el metodo count" do
+            @doublelist.insert(10)
+            @doublelist.insert(20)
+            expect(@doublelist.head.value).to eq(20)
+            expect(@doublelist.tail.value).to eq(10)
+            expect(@doublelist.count).to eq(2)
+        end
+    end
 end
