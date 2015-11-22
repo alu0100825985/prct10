063b4c2e2e8f752319b0ae951656d36f5de639ca
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 18:25:47 +0000

    Expectativa para comprobar el método all? [REALIZADA CON ÉXITO]

--- spec/double_list_spec.rb
+++ spec/double_list_spec.rb
@@ -7,5 +7,13 @@ describe "Pruebas para una lista con nodos simples" do
     end
     
     describe "Haciendo enumerable la lista doblemente enlazada" do
+        before :each do
+            @doublelist.insert(10)
+            @doublelist_empty = Double_List.new(nil,nil)
+        end
+        it "Comprobrando el metodo all?" do
+            expect(@doublelist.all?).to eq(true)
+            expect(@doublelist_empty.all?).to eq(false)
+        end
     end
 end
