5b9988050610aef3505d681a122eef969ffcc45a
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 14:59:22 +0000

    Redefinido el fichero bibliography_spec.rb para crear las expectativas de comparación

--- spec/bibliography_spec.rb
+++ spec/bibliography_spec.rb
@@ -3,36 +3,12 @@ require './lib/bibliography/gem/bibliography.rb'
 
 describe References do
   before :each do
-    @bibliography = Books.new("(The Facets of Ruby)", "4 edition", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
-    @magazine_article = Magazine_Articles.new("PC World", "ISSN-7398473")
-    @newspaper_article = Newspaper_Articles.new("Marca", "National")
-    @electronic_document = Electronic_Document.new("www.marcosruby.es/pdfexport/lessons")
-  end 
-  
-  describe "Jerarquía de Clases - Herencia" do
-    
-    it "Bibliography es una clase herencia de References" do
-      expect(@bibliography.is_a? Books).to eq(true)
-      expect(@bibliography.kind_of? References).to eq(true)
-    end
-    
-    it "Magazine_Articles es una clase herencia de References" do
-      expect(@magazine_article.is_a? Magazine_Articles).to eq(true)
-      expect(@magazine_article.kind_of? References).to eq(true)
-    end
-    
-    it "Newspaper_Articles es una clase herencia de References" do
-      expect(@newspaper_article.is_a? Newspaper_Articles).to eq(true)
-      expect(@newspaper_article.kind_of? References).to eq(true)
-    end
-    
-    it "Electronic_Document es una clase herencia de References" do
-      expect(@electronic_document.is_a? Electronic_Document).to eq(true)
-      expect(@electronic_document.kind_of? References).to eq(true)
-    end
-    
-    it "Probando el tipo" do
-      expect(@electronic_document.respond_to? :issn).to eq(false)
-    end
+    @references = References.new("Ricky Seawton", "Ruby Instances", "Santillanic", "17 July, 2009", "323")
   end
+  
+  context to "Comparaciones entre referencias basadas en el número de páginas" do
+  
+  end
+  
+ 
 end
