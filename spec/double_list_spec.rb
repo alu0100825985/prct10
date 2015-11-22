35145de40aaaeac6477a17ce22fbdb082e0a2424
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-22 20:12:22 +0000

    Expectativa para comprobar el método drop [REALIZADA CON ÉXITO]

--- spec/double_list_spec.rb
+++ spec/double_list_spec.rb
@@ -8,32 +8,36 @@ describe Double_List do
     
     describe "Haciendo enumerable la lista doblemente enlazada" do
         
-        it "Comprobrando el método all?" do
+        it "Comprobando el método all?" do
             @node = Node.new(nil,10,nil)
             @doublelist_all = Double_List.new(@node, @node)
             @doublelist_empty = Double_List.new(nil,nil)
             expect(@doublelist_all.all?).to eq(true)
             expect(@doublelist_empty.all?).to eq(false)
         end
-        it "Comprobrando el método any?" do
+        it "Comprobando el método any?" do
             @node = Node.new(nil,10,nil)
             @doublelist_any = Double_List.new(@node, nil)
             @doublelist_empty = Double_List.new(nil,nil)
             expect(@doublelist_any.any?).to eq(true)
             expect(@doublelist_empty.any?).to eq(false)
         end 
-        it "Comprobrando el método count" do
+        it "Comprobando el método count" do
             @doublelist.insert(10)
             @doublelist.insert(20)
             expect(@doublelist.head.value).to eq(20)
             expect(@doublelist.tail.value).to eq(10)
             expect(@doublelist.count).to eq(2)
         end
-        it "Comprobrando el método detect" do
+        it "Comprobando el método detect" do
             @doublelist.insert([1, 2, 3, 4, 5])
             expect(@doublelist.head.value.detect {|x| x == 3}).to eq(3)
             expect(@doublelist.head.value.find {|x| x == 5}).to eq(5)
             expect(@doublelist.head.value.detect {|x| x.between?(2,5)}).to eq(2)
+        end
+        it "Comprobando el método drop" do
+            @doublelist.insert([1, 2, 3, 4, 5])
+            expect(@doublelist.head.value.drop(3)).to eq([4, 5])
         end
     end
 end
