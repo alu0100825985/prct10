27ffc9096aeb156299546e9d3ef94ba62b9bcb42
alu0100825985 <alu0100825985@ull.edu.es>
2015-11-24 18:58:04 +0000

    Modificada las expectativas para hacerlas con el value de los nodos. Se sustituye last (no existe en el mixin enumerable) por member?

--- spec/double_list_spec.rb
+++ spec/double_list_spec.rb
@@ -4,61 +4,94 @@ require './lib/bibliography/gem/double_list.rb'
 describe Double_List do
     before :each do 
         @doublelist = Double_List.new(nil,nil)
+        @doublelist_alternative = Double_List.new(nil,nil)
     end
     
     describe "Haciendo enumerable la lista doblemente enlazada" do
         
         it "Comprobando el método all?" do
-            @node = Node.new(nil,10,nil)
-            @doublelist_all = Double_List.new(@node, @node)
-            @doublelist_empty = Double_List.new(nil,nil)
-            expect(@doublelist_all.all?).to eq(true)
-            expect(@doublelist_empty.all?).to eq(false)
+            @doublelist.insert(10)
+            @doublelist.insert(20)
+            @doublelist.insert(30)
+            @doublelist_alternative.insert(10)
+            @doublelist_alternative.insert(nil)
+            @doublelist_alternative.insert(30)
+            expect(@doublelist.all?).to eq(true)
+            expect(@doublelist_alternative.all?).to eq(false)
         end
         it "Comprobando el método any?" do
-            @node = Node.new(nil,10,nil)
-            @doublelist_any = Double_List.new(@node, nil)
-            @doublelist_empty = Double_List.new(nil,nil)
-            expect(@doublelist_any.any?).to eq(true)
-            expect(@doublelist_empty.any?).to eq(false)
+            @doublelist.insert(10)
+            @doublelist.insert(20)
+            @doublelist.insert(nil)
+            @doublelist_alternative.insert(nil)
+            @doublelist_alternative.insert(nil)
+            @doublelist_alternative.insert(nil)
+            expect(@doublelist.any?).to eq(true)
+            expect(@doublelist_alternative.any?).to eq(false)
         end 
         it "Comprobando el método count" do
             @doublelist.insert(10)
             @doublelist.insert(20)
-            expect(@doublelist.head.value).to eq(20)
-            expect(@doublelist.tail.value).to eq(10)
-            expect(@doublelist.count).to eq(2)
+            @doublelist.insert(30)
+            @doublelist.insert(40)
+            expect(@doublelist.count).to eq(4)
         end
         it "Comprobando el método detect" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.detect {|x| x == 3}).to eq(3)
-            expect(@doublelist.head.value.find {|x| x == 5}).to eq(5)
-            expect(@doublelist.head.value.detect {|x| x.between?(2,5)}).to eq(2)
+            @doublelist.insert(40)
+            @doublelist.insert(30)
+            @doublelist.insert(20)
+            @doublelist.insert(10)
+            expect(@doublelist.detect {|x| x == 20}).to eq(20)
+            expect(@doublelist.find {|x| x == 40}).to eq(40)
+            expect(@doublelist.detect {|x| x.between?(20,40)}).to eq(20)
         end
         it "Comprobando el método drop" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.drop(3)).to eq([4, 5])
+            @doublelist.insert(40)
+            @doublelist.insert(30)
+            @doublelist.insert(20)
+            @doublelist.insert(10)
+            expect(@doublelist.drop(2)).to eq([30, 40])
         end
         it "Comprobando el método max" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.max).to eq(5)
+            @doublelist.insert(5)
+            @doublelist.insert(4)
+            @doublelist.insert(3)
+            @doublelist.insert(2)
+            @doublelist.insert(1)
+            expect(@doublelist.max).to eq(5)
         end
         it "Comprobando el método min" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.min).to eq(1)
+            @doublelist.insert(5)
+            @doublelist.insert(4)
+            @doublelist.insert(3)
+            @doublelist.insert(2)
+            @doublelist.insert(1)
+            expect(@doublelist.min).to eq(1)
         end
         it "Comprobando el método sort" do
-            @doublelist.insert([4, 1, 5, 3, 2])
-            expect(@doublelist.head.value.sort).to eq([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.sort.reverse).to eq([5, 4, 3, 2, 1])
+            @doublelist.insert(4)
+            @doublelist.insert(2)
+            @doublelist.insert(5)
+            @doublelist.insert(1)
+            @doublelist.insert(3)
+            expect(@doublelist.sort).to eq([1, 2, 3, 4, 5])
+            expect(@doublelist.sort.reverse).to eq([5, 4, 3, 2, 1])
         end
         it "Comprobando el método first" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.first).to eq(1)
+            @doublelist.insert(5)
+            @doublelist.insert(4)
+            @doublelist.insert(3)
+            @doublelist.insert(2)
+            @doublelist.insert(1)
+            expect(@doublelist.first).to eq(1)
         end
-        it "Comprobando el método last" do
-            @doublelist.insert([1, 2, 3, 4, 5])
-            expect(@doublelist.head.value.last).to eq(5)
+        it "Comprobando el método member?" do
+            @doublelist.insert(5)
+            @doublelist.insert(4)
+            @doublelist.insert(3)
+            @doublelist.insert(2)
+            @doublelist.insert(1)
+            expect(@doublelist.member?(3)).to eq(true)
         end
     end
-end
+end 
